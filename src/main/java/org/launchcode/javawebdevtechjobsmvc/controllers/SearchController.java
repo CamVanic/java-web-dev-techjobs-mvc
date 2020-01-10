255925ccd37fc922eea7051fdd21e0c978e752ce
Cam <vanicellic@yahoo.com>
2020-01-09 19:23:06 -0600

    Submitting

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -23,7 +23,7 @@ public class SearchController {
         return "search";
     }
 
-    @RequestMapping("results")
+    @PostMapping("results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         ArrayList<Job> jobs;
         if (searchType.toLowerCase().equals("all")){
